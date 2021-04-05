58fb0af381e91a88ab7c660a50dfa701a5e10ebd
Antti-Juho Nieminen <a-j.nieminen@hotmail.com>
2021-04-05 12:20:54 +0300

    added a new e2e test for a pokemon page

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,4 +6,9 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     );
   });
+  it('particular pokemon can bee accessed from main page', () => {
+    cy.visit('http://localhost:5000');
+    cy.get('a[href*="pokemon/ivysaur"]').click();
+    cy.contains('chlorophyll');
+  });
 });
